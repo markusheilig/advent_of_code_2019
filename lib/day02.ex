a1714e7b4659e706beeffaa26b2987e4d24a8843
Markus Heilig <markusheilig@gmx.net>
2019-12-15 15:40:21 +0100

    try some refactoring

--- lib/day02.ex
+++ lib/day02.ex
@@ -2,74 +2,56 @@ defmodule Day02.IntMachine do
   @op_code_sum 1
   @op_code_mul 2
   @op_code_halt 99
-  @separator ","
+
+  import List, only: [replace_at: 3]
 
   def execute(program) when is_binary(program) do
     program
-    |> binary_to_program()
+    |> parse()
     |> exec()
-    |> to_str()
   end
 
-  def restore(program, noun \\ 12, verb \\ 2) when is_binary(program) do
-    program
-    |> binary_to_program()
-    |> List.replace_at(1, noun)
-    |> List.replace_at(2, verb)
-    |> to_str()
-  end
-
-  def find_nouns_and_verbs_for_value(program, value_to_look_for) do
+  def find_nouns_and_verbs_for_return_code(program, return_code) when is_binary(program) and is_number(return_code) do
     candidates = 0..99
 
     for noun <- candidates,
         verb <- candidates,
-        program = restore(program, noun, verb),
-        program = execute(program),
-        program_starts_with?(program, value_to_look_for) do
+        program = parse(program),
+        program = replace_at(program, 1, noun),
+        program = replace_at(program, 2, verb),
+        exec(program) == return_code do
       {noun, verb}
     end
   end
 
-  defp program_starts_with?(program, value),
-    do: String.starts_with?(program, to_string(value) <> @separator)
-
-  defp binary_to_program(input) do
-    input
-    |> String.split(@separator)
+  defp parse(program) do
+    program
+    |> String.split(",")
     |> Enum.map(&String.to_integer/1)
   end
 
-  defp to_str(program), do: Enum.join(program, @separator)
+  defp exec(program, state \\ :continue, ip \\ 0)
 
-  defp exec(program, state \\ :continue, instruction_pointer \\ 0)
+  defp exec(program, :halt, _ip), do: program |> hd()
 
-  defp exec(program, :continue, instruction_pointer) do
-    opcode = Enum.at(program, instruction_pointer)
-    {new_state, new_program} = exec(opcode, program, instruction_pointer)
-    exec(new_program, new_state, instruction_pointer + 4)
+  defp exec(program, :continue, ip) do
+    opcode = Enum.at(program, ip)
+    {new_state, new_program} = exec(opcode, program, ip)
+    exec(new_program, new_state, ip + 4)
   end
 
-  defp exec(program, :halt, _instruction_pointer), do: program
+  defp exec(@op_code_halt, program, _ip), do: {:halt, program}
 
-  defp exec(@op_code_halt, program, _instruction_pointer), do: {:halt, program}
+  defp exec(@op_code_sum, program, ip), do: continue_apply_fn2(program, ip, &(&1 + &2))
 
-  defp exec(@op_code_sum, program, instruction_pointer) do
-    {param1, param2, pos} = extract_parameters(program, instruction_pointer)
-    new_program = List.replace_at(program, pos, param1 + param2)
-    {:continue, new_program}
-  end
+  defp exec(@op_code_mul, program, ip), do: continue_apply_fn2(program, ip, &(&1 * &2))
 
-  defp exec(@op_code_mul, program, instruction_pointer) do
-    {param1, param2, pos} = extract_parameters(program, instruction_pointer)
-    new_program = List.replace_at(program, pos, param1 * param2)
-    {:continue, new_program}
-  end
-
-  defp extract_parameters(program, instruction_pointer) do
-    [param1_pos, param2_pos, pos] = Enum.slice(program, instruction_pointer + 1, 3)
+  defp continue_apply_fn2(program, ip, fn2) do
+    [param1_pos, param2_pos, pos] = Enum.slice(program, ip + 1, 3)
     param1 = Enum.at(program, param1_pos)
     param2 = Enum.at(program, param2_pos)
-    {param1, param2, pos}
+    new_program = List.replace_at(program, pos, fn2.(param1, param2))
+    {:continue, new_program}
   end
+
 end
--- test/day02_test.exs
+++ test/day02_test.exs
@@ -4,23 +4,23 @@ defmodule Day02Test do
   alias Day02.IntMachine
 
   test "it adds two values given op code 1" do
-    assert IntMachine.execute("1,0,0,0,99") == "2,0,0,0,99"
+    assert IntMachine.execute("1,0,0,0,99") == 2
   end
 
   test "it multiplies two values given op code 2" do
-    assert IntMachine.execute("2,3,0,3,99") == "2,3,0,6,99"
-    assert IntMachine.execute("2,4,4,5,99,0") == "2,4,4,5,99,9801"
+    assert IntMachine.execute("2,3,0,3,99") == 2
+    assert IntMachine.execute("2,4,4,5,99,0") == 2
   end
 
   test "it halts when it reached op code 99" do
-    assert IntMachine.execute("1,1,1,4,99,5,6,0,99") == "30,1,1,4,2,5,6,0,99"
+    assert IntMachine.execute("1,1,1,4,99,5,6,0,99") == 30
   end
 
   test "it solves part 2" do
     program =
       "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,10,23,2,10,23,27,1,27,6,31,1,13,31,35,1,13,35,39,1,39,10,43,2,43,13,47,1,47,9,51,2,51,13,55,1,5,55,59,2,59,9,63,1,13,63,67,2,13,67,71,1,71,5,75,2,75,13,79,1,79,6,83,1,83,5,87,2,87,6,91,1,5,91,95,1,95,13,99,2,99,6,103,1,5,103,107,1,107,9,111,2,6,111,115,1,5,115,119,1,119,2,123,1,6,123,0,99,2,14,0,0"
 
-    [{noun, verb}] = IntMachine.find_nouns_and_verbs_for_value(program, 19_690_720)
+    [{noun, verb}] = IntMachine.find_nouns_and_verbs_for_return_code(program, 19_690_720)
     assert 100 * noun + verb == 6421
   end
 end
