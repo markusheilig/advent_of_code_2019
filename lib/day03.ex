8134e17ece9fab67df29ad22a4a32edf0698b85f
Markus Heilig <markusheilig@gmx.net>
2019-12-16 07:57:22 +0100

    rename 'coordinates' to 'path'

--- lib/day03.ex
+++ lib/day03.ex
@@ -4,30 +4,30 @@ defmodule Day03.Wire do
 
   def nearest_intersection(wire1, wire2) do
     wire1
-    |> coordinates()
-    |> intersection(coordinates(wire2))
+    |> path()
+    |> intersection(path(wire2))
     |> map(&to_manhattan_distance/1)
     |> min()
   end
 
   def nearest_intersection_steps(wire1, wire2) do
-    wire1_coordinates = coordinates(wire1)
-    wire2_coordinates = coordinates(wire2)
+    path_wire1 = path(wire1)
+    path_wire2 = path(wire2)
 
-    intersection_steps =
-      for pos <- intersection(wire1_coordinates, wire2_coordinates) do
-        wire1_steps = Map.get(wire1_coordinates, pos)
-        wire2_steps = Map.get(wire2_coordinates, pos)
-        wire1_steps + wire2_steps
+    distances =
+      for pos <- intersection(path_wire1, path_wire2) do
+        d1 = Map.get(path_wire1, pos)
+        d2 = Map.get(path_wire2, pos)
+        d1 + d2
       end
-    Enum.min(intersection_steps)
+    Enum.min(distances)
   end
 
-  defp coordinates(wire) do
+  defp path(wire) do
     wire
     |> String.split(",")
     |> Enum.map(&to_steps/1)
-    |> Enum.reduce({_start_position = {0, 0}, _coordinates_and_index = %{}}, &walk/2)
+    |> Enum.reduce({_start_position = {0, 0}, _path = %{}}, &walk/2)
     |> elem(1)
   end
 
@@ -36,20 +36,20 @@ defmodule Day03.Wire do
   defp to_steps("U" <> steps), do: {{0, 1}, to_integer(steps)}
   defp to_steps("D" <> steps), do: {{0, -1}, to_integer(steps)}
 
-  defp walk({direction, steps}, {position, coordinates}) do
+  defp walk({direction, steps}, {position, path}) do
     {x, y} = position
     {d1, d2} = direction
 
-    new_coordinates =
+    new_path =
       for step <- 1..steps, into: %{} do
         coordinate = {x + d1 * step, y + d2 * step}
-        index = map_size(coordinates) + step
+        index = map_size(path) + step
         {coordinate, index}
       end
 
     position = {x + d1 * steps, y + d2 * steps}
-    coordinates = Map.merge(coordinates, new_coordinates)
-    {position, coordinates}
+    path = Map.merge(path, new_path)
+    {position, path}
   end
 
   defp to_manhattan_distance({x, y}), do: abs(x) + abs(y)
